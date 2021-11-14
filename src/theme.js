d82530395b7de1528c85ad4c19a83e5cf490c894
asdutoit@gmail.com <asdutoit@gmail.com>
2021-11-14 14:30:09 +0200

    modified theme.js

--- src/theme.js
+++ src/theme.js
@@ -5,10 +5,10 @@ import { red } from "@mui/material/colors";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#ea4336",
+      main: "#b0f566",
     },
     secondary: {
-      main: "#3572F7",
+      main: "#f78ae0",
     },
     error: {
       main: red.A400,
